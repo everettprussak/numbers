0bee783a846ae4001d6f65ae1058ae5709049db8
everettprussak <prussak3@aol.com>
2020-11-18 15:40:16 -0800

    add numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+  
+#numbers.sh
+#Everett Prussak
+
+echo -n "Enter a positive number: "
+read NUM
+N=0
+
+while [ $N -lt "$NUM" ]
+do
+        N=$((N+1))
+        Q=$(( $N % 2))
+        if [ $Q -eq 0 ]
+        then
+                echo $N "Even"
+        else
+                echo $N "Odd"
+        fi
+
+done
+
